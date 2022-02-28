053e79b132f7f702cabc4eb3af4096cbd6256c7f
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-02-28 12:11:30 +0100

    init boilerplate

---
+++ hardhat.config.js
@@ -0,0 +1,49 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+let provider = 'http://localhost:8545'
+let hardhatConfigs = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: provider,
+      chainId: 80001
+    },
+    polygon: {
+      url: provider,
+      chainId: 137
+    }
+  },
+  solidity: "0.8.6",
+}
+
+if (process.env.ACCOUNTS !== undefined) {
+  for (let k in hardhatConfigs.networks) {
+    hardhatConfigs.networks[k].accounts = []
+    for (let a in process.env.ACCOUNTS.split(',')) {
+      if (k === 'hardhat') {
+        hardhatConfigs.networks[k].accounts.push({
+          privateKey: process.env.ACCOUNTS.split(',')[a],
+          balance: "10000000000000000000000000000000000000"
+        })
+      } else {
+        hardhatConfigs.networks[k].accounts.push(process.env.ACCOUNTS.split(',')[a])
+      }
+    }
+  }
+}
+
+if (process.env.PROVIDER !== undefined) {
+  for (let k in hardhatConfigs.networks) {
+    if (k !== 'hardhat') {
+      hardhatConfigs.networks[k].url = process.env.PROVIDER
+    }
+  }
+}
+
+if (process.env.POLYGONSCAN !== undefined) {
+  hardhatConfigs.etherscan = { apiKey: { polygonMumbai: process.env.POLYGONSCAN } }
+}
+
+module.exports = hardhatConfigs;
